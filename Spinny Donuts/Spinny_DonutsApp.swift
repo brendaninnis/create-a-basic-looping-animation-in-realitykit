7652fc2475719c16be689096ed987a344093c0fb
Brendan Innis <innisbrendan@gmail.com>
2023-02-25 20:00:30 -0800

    Initial Commit

---
+++ Spinny Donuts/Spinny_DonutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spinny_DonutsApp.swift
+//  Spinny Donuts
+//
+//  Created by Brendan Innis on 2023-02-25.
+//
+
+import SwiftUI
+
+@main
+struct Spinny_DonutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
